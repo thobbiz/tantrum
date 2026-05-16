4f7e4c1f02cc1e8c7875c3295564669ecbf9f604
thobbiz <odelolatojumi@gmail.com>
2026-05-16 13:21:16 +0100

    feat: added function to loop over and count active backends

--- balancer/balancer.go
+++ balancer/balancer.go
@@ -1,5 +1,7 @@
 package balancer
 
+import "sync/atomic"
+
 type Balancer struct {
 	Backends []*Backend
 	Counter  uint64
@@ -10,4 +12,18 @@ func NewBalancer(backends []*Backend) *Balancer {
 		Backends: backends,
 		Counter:  0,
 	}
+}
+
+func (b *Balancer) NextBackend() *Backend {
+	total := uint64(len(b.Backends))
+
+	for range total {
+		idx := atomic.AddUint64(&b.Counter, 1) % total
+		backend := b.Backends[idx]
+		if backend.IsAlive() {
+			return backend
+		}
+	}
+
+	return nil
 }
