5dd3f3c69af5061e2bee35d9ca03eced40d0c43e
thobbiz <odelolatojumi@gmail.com>
2026-05-16 11:05:33 +0100

    feat: defined set and get methods for backend

---
+++ balancer/backend.go
@@ -0,0 +1,26 @@
+package balancer
+
+import (
+	"net/url"
+	"sync"
+)
+
+type Backend struct {
+	addr  *url.URL
+	alive bool
+	mux   sync.RWMutex
+}
+
+func (B *Backend) SetAlive(alive bool) {
+	B.mux.Lock()
+	defer B.mux.Unlock()
+
+	B.alive = alive
+}
+
+func (B *Backend) IsAlive() bool {
+	B.mux.RLock()
+	defer B.mux.RUnlock()
+	return B.alive
+
+}
